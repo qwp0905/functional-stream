f87989c40c9f1475ebccc7fe6a0db9f74aa02d0b
qwp0905 <qwp0905@gmail.com>
2024-04-23 20:31:56 +0900

    add chain test

---
+++ test/oprerators/chain.spec.ts
@@ -0,0 +1,20 @@
+import { Fs } from '../../src'
+
+describe('chain', () => {
+  it('1', async () => {
+    const result = Fs.range(5).chain([5, 6]).toArray()
+    await expect(result).resolves.toEqual([0, 1, 2, 3, 4, 5, 6])
+  })
+
+  it('2', async () => {
+    const r1 = Fs.range(5).mergeMap(
+      (e) => new Promise<number>((resolve) => setTimeout(() => resolve(e), 100))
+    )
+    const r2 = Fs.range(3)
+      .map((e) => -e)
+      .chain(r1)
+      .toArray()
+
+    await expect(r2).resolves.toEqual([-0, -1, -2, 0, 1, 2, 3, 4])
+  })
+})
