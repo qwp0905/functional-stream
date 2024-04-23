f960630c63dadea3df430d1132facc4a00956c04
qwp0905 <qwp0905@gmail.com>
2024-04-23 20:10:31 +0900

    add test

---
+++ test/oprerators/group-by.spec.ts
@@ -0,0 +1,25 @@
+import { Fs } from '../../src'
+
+describe('group by', () => {
+  it('1', async () => {
+    const result = Fs.range(10)
+      .groupBy((e) => e % 3)
+      .concatMap((e) => e.toArray())
+      .toArray()
+
+    await expect(result).resolves.toEqual([
+      [0, 3, 6, 9],
+      [1, 4, 7],
+      [2, 5, 8]
+    ])
+  })
+
+  it('2', async () => {
+    const result = Fs.range(10)
+      .groupBy((e) => e)
+      .concatAll()
+      .toArray()
+
+    await expect(result).resolves.toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+  })
+})
