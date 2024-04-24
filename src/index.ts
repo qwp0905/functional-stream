2e5a56560e5ac845fb781ec34d9bc072c7360731
qwp0905 <qwp0905@gmail.com>
2024-04-24 20:08:28 +0900

    fix export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export * from './functional-stream'
+export { Fs } from './functional-stream'
 export { Subject, IObserver } from './observer/index'
 export { Pipeline } from './observer/pipeline'
 export { IFs } from './@types/stream'
