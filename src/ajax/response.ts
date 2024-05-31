5491df38d15080ce4872222f0c2144b61ad062c7
qwp0905 <qwp0905@gmail.com>
2024-05-31 22:39:14 +0900

    fix content type

--- src/ajax/response.ts
+++ src/ajax/response.ts
@@ -37,8 +37,14 @@ async function getBody(res: Response, type?: ResponseType): Promise<any> {
   if (!res.body) {
     return {}
   }
-  const content_type = res.headers.get('content-type')
-  const charset = content_type?.replace(/^.*charset=([^;]*);?.*$/i, '$1')
+
+  const content_type_header = res.headers.get('content-type')?.split(';')
+  const charset = content_type_header
+    ?.find((e) => e.toLowerCase().startsWith('charset='))
+    ?.replace(/^charset=/, '')
+  const content_type = content_type_header?.find(
+    (e) => !e.toLowerCase().startsWith('charset=')
+  )
 
   switch (type) {
     case 'json':
@@ -53,33 +59,22 @@ async function getBody(res: Response, type?: ResponseType): Promise<any> {
       return Fs.from(res.body.pipeThrough(new TextDecoderStream(charset)))
   }
 
-  if (!content_type) {
-    return res.text()
+  switch (content_type) {
+    case 'application/json':
+      return res.json()
+    case 'application/x-ndjson':
+      return Fs.from(res.body.pipeThrough(new TextDecoderStream(charset))).split('\n')
+    case 'application/octet-stream':
+      return Fs.from(res.body.pipeThrough(new TextDecoderStream(charset)))
+    case 'application/x-www-form-urlencode':
+      const body: Record<string, string> = {}
+      for (const [k, v] of new URLSearchParams(await res.text()).entries()) {
+        body[k] = v
+      }
+      return body
+    case 'multipart/formed-data':
+      return res.formData()
+    default:
+      return res.text()
   }
-
-  if (/application\/json/i.test(content_type)) {
-    return res.json()
-  }
-
-  if (/application\/x-ndjson/i.test(content_type)) {
-    return Fs.from(res.body.pipeThrough(new TextDecoderStream(charset))).split('\n')
-  }
-
-  if (/application\/octet-stream/i.test(content_type)) {
-    return Fs.from(res.body.pipeThrough(new TextDecoderStream(charset)))
-  }
-
-  if (/application\/x-www-form-urlencode/i.test(content_type)) {
-    const body: Record<string, string> = {}
-    for (const [k, v] of new URLSearchParams(await res.text()).entries()) {
-      body[k] = v
-    }
-    return body
-  }
-
-  if (/multipart\/formed-data/i.test(content_type)) {
-    return res.formData()
-  }
-
-  return res.text()
 }
