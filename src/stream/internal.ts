6c74d5a62f3a54c15bc7542c655bb95ee5bbd6e9
qwp0905 <qwp0905@gmail.com>
2024-06-16 16:16:51 +0900

    fix throttle operator

--- src/stream/internal.ts
+++ src/stream/internal.ts
@@ -374,7 +374,7 @@ export class FsInternal<T> implements IFs<T> {
       .mergeMap((e) =>
         Fs.from<T>(callback(e) as any)
           .startWith(e)
-          .takeWhile((_, i) => i.equal(0))
+          .takeWhile(() => false)
           .finalize(() => (blocked = false))
       )
   }
