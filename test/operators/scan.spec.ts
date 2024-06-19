b8533e47431262cb81654975991018b675e62de0
qwp0905 <qwp0905@gmail.com>
2024-06-19 19:35:34 +0900

    add scan test

--- test/operators/scan.spec.ts
+++ test/operators/scan.spec.ts
@@ -8,4 +8,11 @@ describe('scan', () => {
 
     await expect(r).resolves.toStrictEqual([[0], [0, 1], [0, 1, 2]])
   })
+
+  it('2', async () => {
+    const r = Fs.range(5, 1)
+      .scan((a, c) => a + c)
+      .toArray()
+    await expect(r).resolves.toStrictEqual([1, 3, 6, 10, 15])
+  })
 })
