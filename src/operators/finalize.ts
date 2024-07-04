82817e9e6c28002d2548bdbe6a19a483a96e0eb1
qwp0905 <qwp0905@gmail.com>
2024-07-04 19:39:33 +0900

    fix finalizer;

--- src/operators/finalize.ts
+++ src/operators/finalize.ts
@@ -2,11 +2,11 @@ import { OperatorPipe, IFunction0 } from "../index.js"
 
 export const finalize = <T>(callback: IFunction0<void>): OperatorPipe<T> => {
   return (source, dest) => {
-    dest.add(callback)
     source.watch({
       next: dest.publish.bind(dest),
       error: dest.abort.bind(dest),
-      complete: dest.commit.bind(dest)
+      complete: dest.commit.bind(dest),
+      finalize: callback
     })
   }
 }
